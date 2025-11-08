2cfc01372bea2c3c4972fa5825e2ac63e5488128
camila sosa <camila.msosa99@gmail.com>
2025-11-08 12:34:37 -0300

    creo funcion solicitarDatosUsuario

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ const recetas = [
 
 let recetasSeleccionadas = [];
 let nombreUsuario = "";
+
+
+//FUNCIONES
+
+// Función 1: Solicitar información al usuario
+
+function solicitarDatosUsuario() {
+    nombreUsuario = prompt("¡Bienvenido al simulador de recetas vegetarianas!\n¿Cuál es tu nombre?");
+
+    console.log("\nHola " + nombreUsuario + ", ¡empecemos a buscar recetas perfectas para vos!\n");
+}
+
+solicitarDatosUsuario();
