f0292f874cfda79f398e4c5aab6d5a10a3d3ee5b
camila sosa <camila.msosa99@gmail.com>
2025-11-08 11:09:34 -0300

    armo constantesy variables

---
+++ script.js
@@ -0,0 +1,11 @@
+//CONSTANTES Y VARIABLES
+const recetas = [
+    { nombre: "Hamburguesa de lentejas", tiempo: 30, dificultad: "Media", vegano: true },
+    { nombre: "Pastel de papas", tiempo: 45, dificultad: "Fácil", vegano: false },
+    { nombre: "Tacos de garbanzos", tiempo: 20, dificultad: "Fácil", vegano: true },
+    { nombre: "Lasaña de verduras", tiempo: 60, dificultad: "Difícil", vegano: false },
+    { nombre: "Ensalada Mediterranea", tiempo: 15, dificultad: "Muy fácil", vegano: true }
+];
+
+let recetasSeleccionadas = [];
+let nombreUsuario = "";
